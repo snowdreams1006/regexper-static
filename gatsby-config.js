3ce3a886ed3691b1968b6c6d41bded2cb8754829
Jeff Avallone <jeff.avallone@gmail.com>
2019-01-03 18:00:10 -0500

    Loading font through CSS to avoid font loading flash

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,15 +11,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-postcss',
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Bangers'],
-          text: 'Regxpr'
-        }
-      }
-    }
+    'gatsby-plugin-postcss'
   ]
 };
