533475e6136cd1b4e3f669adcc3c84635ecd4b12
Jeff Avallone <jeff.avallone@gmail.com>
2019-01-03 18:01:01 -0500

    Adding Message

---
+++ src/components/Message/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import style from './style.module.css';
+
+import InfoIcon from 'react-feather/dist/icons/info';
+import ErrorIcon from 'react-feather/dist/icons/alert-octagon';
+import WarningIcon from 'react-feather/dist/icons/alert-triangle';
+
+const iconTypes = {
+  info: InfoIcon,
+  error: ErrorIcon,
+  warning: WarningIcon
+};
+
+const renderIcon = (type, icon) => {
+  const Icon = icon || iconTypes[type];
+
+  if (!Icon) {
+    return;
+  }
+
+  return <Icon />;
+};
+
+const Message = ({ type, icon, heading, children }) => (
+  <div className={ [style.message, type && style[type] ].filter(Boolean).join(' ') }>
+    <div className={ style.header }>
+      { renderIcon(type, icon) }
+      <h2>{ heading }</h2>
+    </div>
+    <div className={ style.content }>
+      { children }
+    </div>
+  </div>
+);
+
+Message.propTypes = {
+  type: PropTypes.oneOf([
+    'info',
+    'error',
+    'warning'
+  ]),
+  icon: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.func
+  ]),
+  heading: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+};
+
+export default Message;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from 'components/Layout';
+import Message from 'components/Message';
 
 const IndexPage = () => <Layout>
+  <noscript>
+    <Message type="error" heading="JavaScript Required">
+      <p>You need JavaScript to use Regexper.</p>
+      <p>If you have concerns regarding the use of tracking code on Regexper, please see the <Link to="/privacy">Privacy Policy</Link>.</p>
+    </Message>
+  </noscript>
   <div>Hello world</div>
 </Layout>;
 
