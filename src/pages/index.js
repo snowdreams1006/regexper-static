bbdc5a3b12279fe8bc86a30006d750c3cf6f9ef1
Jeff Avallone <jeff.avallone@gmail.com>
2019-01-15 21:19:28 -0500

    Reverting PWA install prompt
    This protocol changed from when the old React implementation was built
    and it doesn't work from a user-experience perspective now

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import URLSearchParams from '@ungap/url-search-params';
 import Metadata from 'components/Metadata';
 import Message from 'components/Message';
 import App from 'components/App';
-import InstallPrompt from 'components/InstallPrompt';
 
 export const query = graphql`
   query IndexPageQuery {
@@ -40,81 +39,36 @@ const readURLHash = (location, defaultSyntax) => {
   }
 };
 
-class IndexPage extends React.PureComponent {
-  state={
-    installPrompt: null
-  }
+export const IndexPage = ({
+  location,
+  data: { site: { siteMetadata } }
+}) => <>
+  <Metadata/>
+  <noscript>
+    <Message type="error" heading="JavaScript Required">
+      <p>You need JavaScript to use Regexper.</p>
+      <p>If you have concerns regarding the use of tracking code on Regexper,
+        please see the <Link to="/privacy">Privacy Policy</Link>.</p>
+    </Message>
+  </noscript>
+  <App
+    syntaxList={ siteMetadata.syntaxList }
+    { ...readURLHash(location, siteMetadata.defaultSyntax) } />
+</>;
 
-  static propTypes = {
-    location: PropTypes.object,
-    data: PropTypes.shape({
-      site: PropTypes.shape({
-        siteMetadata: PropTypes.shape({
-          defaultSyntax: PropTypes.string,
-          syntaxList: PropTypes.arrayOf(PropTypes.shape({
-            id: PropTypes.string,
-            label: PropTypes.string
-          }))
-        })
+IndexPage.propTypes = {
+  location: PropTypes.object,
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        defaultSyntax: PropTypes.string,
+        syntaxList: PropTypes.arrayOf(PropTypes.shape({
+          id: PropTypes.string,
+          label: PropTypes.string
+        }))
       })
     })
-  }
-
-  componentDidMount() {
-    window.addEventListener('beforeinstallprompt', this.handleInstallPrompt);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('beforeinstallprompt', this.handleInstallPrompt);
-  }
-
-  handleInstallPrompt = event => {
-    event.preventDefault();
-
-    this.setState({
-      installPrompt: event
-    });
-  }
-
-  handleInstallReject = () => {
-    this.setState({ installPrompt: null });
-  }
-
-  handleInstallAccept = () => {
-    const { installPrompt } = this.state;
-
-    this.setState({ installPrompt: null });
-    installPrompt.prompt();
-  }
-
-  render() {
-    const {
-      installPrompt
-    } = this.state;
-    const {
-      location,
-      data: { site: { siteMetadata } }
-    } = this.props;
-
-    return <>
-      <Metadata/>
-      <noscript>
-        <Message type="error" heading="JavaScript Required">
-          <p>You need JavaScript to use Regexper.</p>
-          <p>
-            If you have concerns regarding the use of tracking code on Regexper,
-            please see the <Link to="/privacy">Privacy Policy</Link>.
-          </p>
-        </Message>
-      </noscript>
-      <App
-        syntaxList={ siteMetadata.syntaxList }
-        { ...readURLHash(location, siteMetadata.defaultSyntax) } />
-      { installPrompt && <InstallPrompt
-        onAccept={ this.handleInstallAccept }
-        onReject={ this.handleInstallReject } /> }
-    </>;
-  }
-}
+  })
+};
 
 export default IndexPage;
--- src/pages/index.test.js
+++ src/pages/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import IndexPage from 'pages/index';
+import { IndexPage } from 'pages/index';
 
 const queryResult = {
   site: {
@@ -31,74 +31,5 @@ describe('Index Page', () => {
       }} data={ queryResult } />
     );
     expect(component).toMatchSnapshot();
-  });
-
-  test('rendering after an install prompt has been requested', () => {
-    const component = shallow(
-      <IndexPage location={{ hash: '' }} data={ queryResult } />
-    );
-    expect(component).toMatchSnapshot();
-
-    component.instance().handleInstallPrompt({
-      preventDefault: jest.fn(),
-      prompt: jest.fn()
-    });
-
-    expect(component).toMatchSnapshot();
-  });
-
-  test('removing event listener on umount', () => {
-    jest.spyOn(window, 'addEventListener');
-    jest.spyOn(window, 'removeEventListener');
-
-    const component = shallow(
-      <IndexPage location={{ hash: '' }} data={ queryResult } />
-    );
-
-    expect(window.addEventListener).toHaveBeenCalledWith(
-      'beforeinstallprompt',
-      expect.any(Function));
-
-    component.unmount();
-
-    expect(window.removeEventListener).toHaveBeenCalledWith(
-      'beforeinstallprompt',
-      expect.any(Function));
-  });
-
-  test('rejecting install prompt', () => {
-    const component = shallow(
-      <IndexPage location={{ hash: '' }} data={ queryResult } />
-    );
-    const instance = component.instance();
-    const installEvent = {
-      preventDefault: jest.fn(),
-      prompt: jest.fn()
-    };
-
-    instance.handleInstallPrompt(installEvent);
-
-    instance.handleInstallReject();
-
-    expect(installEvent.prompt).not.toHaveBeenCalled();
-    expect(component.state('installPrompt')).toEqual(null);
-  });
-
-  test('accepting install prompt', () => {
-    const component = shallow(
-      <IndexPage location={{ hash: '' }} data={ queryResult } />
-    );
-    const instance = component.instance();
-    const installEvent = {
-      preventDefault: jest.fn(),
-      prompt: jest.fn()
-    };
-
-    instance.handleInstallPrompt(installEvent);
-
-    instance.handleInstallAccept();
-
-    expect(installEvent.prompt).toHaveBeenCalled();
-    expect(component.state('installPrompt')).toEqual(null);
   });
 });
