325f01f03487d64299f1955925abb192dfbe32ec
Jeff Avallone <jeff.avallone@gmail.com>
2019-01-15 21:57:43 -0500

    Translating Loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withNamespaces } from 'react-i18next';
 
 import LoaderIcon from 'react-feather/dist/icons/loader';
 
 import style from './style.module.css';
 
-const Loader = () => (
+const Loader = ({ t }) => (
   <div className={ style.loader }>
     <LoaderIcon />
-    <div className={ style.message }>Loading...</div>
+    <div className={ style.message }>{ t('Loading...') }</div>
   </div>
 );
 
-export default Loader;
+Loader.propTypes = {
+  t: PropTypes.func.isRequired
+};
+
+export { Loader };
+export default withNamespaces()(Loader);
--- src/components/Loader/test.js
+++ src/components/Loader/test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import Loader from 'components/Loader';
+import { mockT } from 'i18n';
+import { Loader } from 'components/Loader';
 
 describe('Loader', () => {
   test('rendering', () => {
     // Using full rendering here since styles for this depend on the structure
     // of the SVG.
     const component = mount(
-      <Loader />
+      <Loader t={ mockT } />
     );
     expect(component).toMatchSnapshot();
   });
