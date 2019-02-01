a13f26286d34fa341d38e3e4b249d492cb9717f3
Jeff Avallone <jeff.avallone@gmail.com>
2019-01-31 21:28:31 -0500

    Moving quadratic curve helper to be owned by VerticalLayout

--- src/rendering/VerticalLayout/index.js
+++ src/rendering/VerticalLayout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import QuadraticCurve from 'quadratic-curve';
+import QuadraticCurve from './quadratic-curve';
 
 import * as style from 'rendering/style';
 
