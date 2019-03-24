ced6c42c3d826ae3f7c6392d6ba80b8845acd404
Jeff Avallone <jeff.avallone@gmail.com>
2019-03-24 13:02:22 -0400

    Updating PrivacyPolicy to use react-testing-library

--- src/components/PrivacyPolicy/test.js
+++ src/components/PrivacyPolicy/test.js
@@ -1,14 +1,17 @@
+jest.mock('components/Message', () =>
+  require('__mocks__/component-mock')('components/Message'));
+
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 
 import { mockT } from 'i18n';
 import { PrivacyPolicy } from 'components/PrivacyPolicy';
 
 describe('PrivacyPolicy', () => {
   test('rendering', () => {
-    const component = shallow(
+    const { asFragment } = render(
       <PrivacyPolicy onClose={ jest.fn() } t={ mockT } />
     );
-    expect(component).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
