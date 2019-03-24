e29b40990c53efa1416be815f5140f66c4343770
Jeff Avallone <jeff.avallone@gmail.com>
2019-03-24 13:18:29 -0400

    Updating SentryBoundary to use react-testing-library

--- src/components/SentryBoundary/test.js
+++ src/components/SentryBoundary/test.js
@@ -1,41 +1,53 @@
 jest.mock('@sentry/browser');
+jest.mock('components/SentryError', () =>
+  require('__mocks__/component-mock')('components/SentryError'));
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 import * as Sentry from '@sentry/browser';
 
 import SentryBoundary from 'components/SentryBoundary';
 
 describe('SentryBoundary', () => {
   test('rendering', () => {
-    const component = shallow(
+    const { asFragment } = render(
       <SentryBoundary>
         Example content
       </SentryBoundary>
     );
-    expect(component).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('error handling', () => {
-    const Child = () => 'Example content';
-    const component = shallow(
+    // Hide React's console logging about the error
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const error =new Error('Example error');
+    const Child = ({ shouldThrow }) => {
+      if (shouldThrow) {
+        throw error;
+      }
+
+      return 'Example content';
+    };
+    const { asFragment, rerender } = render(
       <SentryBoundary>
-        <Child />
+        <Child shouldThrow={ false } />
       </SentryBoundary>
     );
-    expect(component).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
 
-    const error = new Error('Example error');
-    component.find('Child').simulateError(error);
-    // NOTE: Enzyme doesn't call getDerivedStateFromError yet, so we have to
-    // set the state manually
-    component.setState(SentryBoundary.getDerivedStateFromError(error));
+    rerender(
+      <SentryBoundary>
+        <Child shouldThrow={ true } />
+      </SentryBoundary>
+    );
 
     const scope = { setExtra: jest.fn() };
     expect(Sentry.withScope).toHaveBeenCalled();
 
     Sentry.withScope.mock.calls[0][0](scope);
     expect(Sentry.captureException).toHaveBeenCalledWith(error);
-    expect(component).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
