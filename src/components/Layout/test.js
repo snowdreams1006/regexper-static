ace907779fbe3a9ae1d1c1371d42c975623bc43f
Jeff Avallone <jeff.avallone@gmail.com>
2019-03-24 13:06:11 -0400

    Updating Layout to use react-testing-library

--- src/components/Layout/test.js
+++ src/components/Layout/test.js
@@ -1,15 +1,22 @@
+jest.mock('components/SentryBoundary', () =>
+  require('__mocks__/component-mock')('components/SentryBoundary'));
+jest.mock('components/Header', () =>
+  require('__mocks__/component-mock')('components/Header'));
+jest.mock('components/Footer', () =>
+  require('__mocks__/component-mock')('components/Footer'));
+
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 
 import { Layout } from 'components/Layout';
 
 describe('Layout', () => {
   test('rendering', () => {
-    const component = shallow(
+    const { asFragment } = render(
       <Layout banner="Test Banner" buildId="test-buildid">
         Example content
       </Layout>
     );
-    expect(component).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
