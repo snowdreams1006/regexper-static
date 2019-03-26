280412e2db5e697261b841691f4263bb654d8dfc
Jeff Avallone <jeff.avallone@gmail.com>
2019-03-26 18:26:20 -0400

    Mocking the useTranslation hook

--- src/__mocks__/react-i18next.js
+++ src/__mocks__/react-i18next.js
@@ -1,6 +1,8 @@
 const reactI18next = jest.requireActual('react-i18next');
+const i18n = require('i18n');
 
 module.exports = {
   ...reactI18next,
-  Trans: require('__mocks__/component-mock').buildMock(reactI18next.Trans)
+  Trans: require('__mocks__/component-mock').buildMock(reactI18next.Trans),
+  useTranslation: () => ({ i18n, t: i18n.mockT })
 };
